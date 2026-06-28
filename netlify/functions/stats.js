41eebf508e8dd3eeac179ed98cfaa903f43765f7
donniedarko44 <donat.batarilo@gmail.com>
2026-06-28 20:47:04 +0200

    Update API season parameter from 2025 to 2024

--- netlify/functions/stats.js
+++ netlify/functions/stats.js
@@ -7,7 +7,7 @@ exports.handler = async function(event) {
 
   try {
     const res = await fetch(
-      `https://v3.football.api-sports.io/players?id=${id}&season=2025`,
+      `https://v3.football.api-sports.io/players?id=${id}&season=2024`,
       {
         method: "GET",
         headers: { "x-apisports-key": process.env.API_FOOTBALL_KEY }
